4b61204b03b1e16dd210465a228b58f339fef4d8
Pratik Bhagat <bhagatpratik.2002@gmail.com>
2021-08-07 20:05:50 +0530

    added routes on homepage

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Create from "./components/create";
 import "./App.css";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Link, Route } from "react-router-dom";
 import Read from "./components/read";
 import Update from "./components/update";
 
@@ -17,6 +17,19 @@ function App() {
           </div>
 
           <Route path="/update" component={Update} />
+        </div>
+        <div>
+          <ul>
+            <Link to="/create">
+              <li>Create</li>
+            </Link>
+            <Link to="/read">
+              <li>Read</li>
+            </Link>
+            <Link to="/update">
+              <li>Update</li>
+            </Link>
+          </ul>
         </div>
       </div>
     </Router>
